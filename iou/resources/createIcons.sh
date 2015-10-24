eba9bf7ea36bdb52a5cb07b731d8a96f77a211f5
Knut Nygaard <knutnyg@gmail.com>
2015-10-24 11:31:14 +0200

    svg master icon

---
+++ iou/resources/createIcons.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+rsvg-convert -h 29 calculator61.svg > icon29.png
+rsvg-convert -h 58 calculator61.svg > icon58.png
+rsvg-convert -h 87 calculator61.svg > icon87.png
+rsvg-convert -h 80 calculator61.svg > icon80.png
+rsvg-convert -h 120 calculator61.svg > icon120.png
+rsvg-convert -h 57 calculator61.svg > icon57.png
+rsvg-convert -h 114 calculator61.svg > icon114.png
+rsvg-convert -h 180 calculator61.svg > icon180.png
+rsvg-convert -h 40 calculator61.svg > icon40.png
+rsvg-convert -h 50 calculator61.svg > icon50.png
+rsvg-convert -h 100 calculator61.svg > icon100.png
+rsvg-convert -h 72 calculator61.svg > icon72.png
+rsvg-convert -h 144 calculator61.svg > icon144.png
+rsvg-convert -h 76 calculator61.svg > icon76.png
+rsvg-convert -h 152 calculator61.svg > icon152.png
