cca7f4989d5b9f85e6d4c01730f6021ae0998233
Knut Nygaard <knutnyg@gmail.com>
2016-03-01 17:31:49 +0100

    Refactor tests

--- iouTests/ExpenseTests.swift
+++ iouTests/ExpenseTests.swift
@@ -9,83 +9,15 @@
 import Foundation
 import XCTest
 
-class ExpenseTests: XCTestCase {
-
-    var users: [User]!
-    var user: User!
-    var group: Group!
-    var token: String!
-
-    var runIntegrationTests = false
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+class ExpenseTests: TestBase {
 
     func testExpenseListFetch() {
 
         let expectation = expectationWithDescription("promise")
 
-        GroupHandler.getGroup(APITests.token, group: group)
+        GroupHandler.getGroup(token, group: group)
         .onSuccess {
             group in
-            XCTAssert(true)
-            expectation.fulfill()
-        }
-        .onFailure {
-            error in
-            XCTAssert(false)
-            expectation.fulfill()
-        }
-
-        waitForExpectationsWithTimeout(5, handler: {
-            error in
-            XCTAssertNil(error, "Error")
-        })
-    }
-
-    func testExpenseUpdate() {
-        if runIntegrationTests == false {
-            return
-        }
-
-        let expectation = expectationWithDescription("promise")
-
-        let expenseAmount = Double(arc4random_uniform(150))
-
-        let expense = Expense(participants: [user], amount: expenseAmount, date: NSDate(), groupId: group.id, created: NSDate(), updated: NSDate(), comment: "Updated from test", creator: user, id: "1257f0ef-fc80-486b-901a-9fe609271fa1")
-
-        ExpensesHandler.updateExpense(APITests.token, expense: expense)
-        .onSuccess {
-            expense in
-            XCTAssertTrue(expense.amount == expenseAmount)
-            expectation.fulfill()
-        }
-        .onFailure {
-            error in
-            XCTAssert(false)
-            expectation.fulfill()
-        }
-
-        waitForExpectationsWithTimeout(5, handler: {
-            error in
-            XCTAssertNil(error, "Error")
-        })
-    }
-
-    func testDeleteExpense() {
-        if runIntegrationTests == false {
-            return
-        }
-
-        let expectation = expectationWithDescription("promise")
-
-        let expense = Expense(participants: [user], amount: 512, date: NSDate(), groupId: group.id, created: NSDate(), updated: NSDate(), comment: "Edited from test", creator: user, id: user.id)
-
-        ExpensesHandler.deleteExpense(APITests.token, expense: expense)
-        .onSuccess {
-            success in
             XCTAssert(true)
             expectation.fulfill()
         }
@@ -108,7 +40,7 @@ class ExpenseTests: XCTestCase {
         let editedExpense = Expense(participants: [user], amount: 512, date: NSDate(), groupId: group.id, created: NSDate(), updated: NSDate(), comment: "Edited from test", creator: user)
 
         //Create expense
-        ExpensesHandler.newExpense(APITests.token, expense: newExpense)
+        ExpensesHandler.newExpense(token, expense: newExpense)
         .onSuccess {
             expense in
             editedExpense.id = expense.id
@@ -129,7 +61,7 @@ class ExpenseTests: XCTestCase {
         //Update expense
         expectation = expectationWithDescription("edit")
 
-        ExpensesHandler.updateExpense(APITests.token, expense: editedExpense)
+        ExpensesHandler.updateExpense(token, expense: editedExpense)
         .onSuccess {
             expense in
             XCTAssertTrue(expense.amount == 512)
@@ -149,7 +81,7 @@ class ExpenseTests: XCTestCase {
         //Delete expense
         expectation = expectationWithDescription("delete")
 
-        ExpensesHandler.deleteExpense(APITests.token, expense: editedExpense)
+        ExpensesHandler.deleteExpense(token, expense: editedExpense)
         .onSuccess {
             success in
             XCTAssert(true)
@@ -166,34 +98,4 @@ class ExpenseTests: XCTestCase {
             XCTAssertNil(error, "Error")
         })
     }
-
-    func testCreateExpense() {
-
-        if runIntegrationTests == false {
-            return
-        }
-
-        let expectation = expectationWithDescription("promise")
-
-        let expense = Expense(participants: [user], amount: 0, date: NSDate(), groupId: group.id, created: NSDate(), updated: NSDate(), comment: "Receipe from test", creator: user, id: "1af102d9-c224-451b-b793-7a239af09807")
-
-        ExpensesHandler.newExpense(APITests.token, expense: expense)
-        .onSuccess {
-            success in
-            XCTAssert(true)
-            expectation.fulfill()
-        }
-        .onFailure {
-            error in
-            XCTAssert(false)
-            expectation.fulfill()
-        }
-
-        waitForExpectationsWithTimeout(5, handler: {
-            error in
-            XCTAssertNil(error, "Error")
-        })
-    }
-
-
 }
--- iouTests/GroupHandlerTests.swift
+++ iouTests/GroupHandlerTests.swift
@@ -9,23 +9,8 @@
 import Foundation
 import XCTest
 
-class GroupListFetcherTests : XCTestCase {
-    
-    var users:[User]!
-    var user:User!
-    var group:Group!
-    var token:String!
+class GroupListFetcherTests : TestBase {
 
-    var runIntegrationTests = false
-
-    override func setUp() {
-        super.setUp()
-        user = User(name: "Knut Nygaard", shortName: "Knut", id: "1af102d9-c224-451b-b793-7a239af09807", photoUrl: "https://lh3.googleusercontent.com/-f-ipeFeTcOo/AAAAAAAAAAI/AAAAAAAAAEw/C_qopDlJom4/photo.jpg?sz=50", email:"knutnyg@gmail.com")
-        users = [user]
-        group = Group(members: users, id: "dc0d4b89-4cfb-4c99-8a25-3ac39e6ff559", archived: false, created: NSDate(), description: "IOS", lastUpdated: NSDate(), creator: user, expenses: [])
-        token = APITests.token
-    }
-    
     func testFetchGroups(){
 
         let expectation = expectationWithDescription("promise")
@@ -44,56 +29,6 @@ class GroupListFetcherTests : XCTestCase {
             XCTAssertNil(error, "Error")
         })
     }
-    
-    func testCreateGroup(){
-
-        print("testing createa group")
-
-        if runIntegrationTests == false {
-            return
-        }
-        
-        let expectation = expectationWithDescription("promise")
-        
-        let inputGroup:Group = group
-        
-        GroupHandler.createGroup(token, name: group.description, creator: group.creator)
-            .onSuccess { outputGroup in
-                print("made groups")
-                XCTAssertTrue(outputGroup.id != nil)
-                XCTAssertTrue(outputGroup.description == inputGroup.description)
-                expectation.fulfill()
-            }
-            .onFailure { error in
-                XCTAssert(false)
-                expectation.fulfill()
-            }
-        waitForExpectationsWithTimeout(5, handler: { error in
-            XCTAssertNil(error, "Error")})
-    }
-
-    func testEditGroup(){
-
-        if runIntegrationTests == false {
-            return
-        }
-
-        let expectation = expectationWithDescription("promise")
-
-        let group:Group = self.group
-        group.description = "Test-group-" + NSDate().shortPrintable()
-
-        GroupHandler.putGroup(token, group:group)
-            .onSuccess { return_group in
-                XCTAssertTrue(group.description == return_group.description)
-                expectation.fulfill()
-            }.onFailure { error in
-                XCTAssert(false)
-                expectation.fulfill()
-        }
-        waitForExpectationsWithTimeout(5, handler: { error in
-            XCTAssertNil(error, "Error")})
-    }
 
     func testCreateEditAndArchiveGroup(){
         //Create
@@ -101,7 +36,7 @@ class GroupListFetcherTests : XCTestCase {
 
         var createdGroup = Group()
 
-        GroupHandler.createGroup(APITests.token, name: "Test Group", creator: APITests.user)
+        GroupHandler.createGroup(token, name: "Test Group", creator: user)
         .onSuccess { group in
             XCTAssertTrue(group.id != nil)
             createdGroup = group
@@ -119,7 +54,7 @@ class GroupListFetcherTests : XCTestCase {
 
         createdGroup.description = "Test Group edited!"
 
-        GroupHandler.putGroup(APITests.token, group: createdGroup)
+        GroupHandler.putGroup(token, group: createdGroup)
         .onSuccess { group in
             XCTAssertTrue(group.description == createdGroup.description)
             expectation.fulfill()
@@ -136,7 +71,7 @@ class GroupListFetcherTests : XCTestCase {
         createdGroup.description = "Test-group-archived \(NSDate().shortPrintable())"
         createdGroup.archived = true
 
-        GroupHandler.putGroup(APITests.token, group: createdGroup)
+        GroupHandler.putGroup(token, group: createdGroup)
         .onSuccess { group in
             XCTAssertTrue(group.description == createdGroup.description)
             XCTAssertTrue(group.archived == true)
